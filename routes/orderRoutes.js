1336273fb9b8023bffc2f134873974fbbd580eb5
crvwler <aaababuji@gmail.com>
2024-12-02 11:11:39 +0100

    Mod order routes, to include GET request

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,6 +4,7 @@ const Order = require("../models/Order");
 
 // POST a new order
 router.post("/", async (req, res) => {
+  console.log("Order route hit");
   try {
     const { name, phone, lessonIDs, numberOfSpaces } = req.body;
 
@@ -19,6 +20,16 @@ router.post("/", async (req, res) => {
     res.status(201).json(newOrder);
   } catch (err) {
     res.status(400).send("Bad Request");
+  }
+});
+
+// GET all orders
+router.get("/", async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).send("Error retrieving orders");
   }
 });
 
